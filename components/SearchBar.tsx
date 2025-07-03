028ebcd60b2a275a98d58d6f57664e9aebbdc9f5
Laxman Suthar <laxmansuthar365@gmail.com>
2025-07-03 22:54:51 +0530

    implement tab navigation

---
+++ components/SearchBar.tsx
@@ -0,0 +1,28 @@
+import { icons } from '@/constants/icons';
+import React from 'react';
+import { Image, StyleSheet, TextInput, View } from 'react-native';
+
+interface Props {
+    placeholder:string,
+    onPress:()=>void;
+}
+
+const SearchBar = ({onPress, placeholder}) => {
+  return (
+    <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
+      <Image source={icons.search} className='size-5' resizeMode='contain' tintColor={"#AD8BFF"}/>
+      <TextInput 
+        onPress={()=>{}}
+        placeholder={placeholder}
+        className='text-white text-lg ml-3'
+        value=''
+        onChangeText={()=>{}}
+        placeholderTextColor={"#A8B5DB"}
+      />
+    </View>
+  )
+}
+
+export default SearchBar
+
+const styles = StyleSheet.create({})
