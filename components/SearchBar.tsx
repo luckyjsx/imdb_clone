1e3f9c5e5bb941e2398399b75aaae6d8b40474a3
Laxman Suthar <laxmansuthar365@gmail.com>
2025-07-05 23:57:55 +0530

    home screen ui

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,20 +3,22 @@ import React from 'react';
 import { Image, StyleSheet, TextInput, View } from 'react-native';
 
 interface Props {
-    placeholder:string,
-    onPress:()=>void;
+  placeholder: string,
+  onPress: () => void,
+  value:string,
+  onChangeText:(text:string)=>void
 }
 
-const SearchBar = ({onPress, placeholder}) => {
+const SearchBar = ({ onPress, placeholder,value ,onChangeText}:Props) => {
   return (
     <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
-      <Image source={icons.search} className='size-5' resizeMode='contain' tintColor={"#AD8BFF"}/>
-      <TextInput 
-        onPress={()=>{}}
+      <Image source={icons.search} className='size-5' resizeMode='contain' tintColor={"#AD8BFF"} />
+      <TextInput
+        value={value}
+        onChangeText={onChangeText}
+        onPress={onPress}
         placeholder={placeholder}
         className='text-white text-lg ml-3'
-        value=''
-        onChangeText={()=>{}}
         placeholderTextColor={"#A8B5DB"}
       />
     </View>
